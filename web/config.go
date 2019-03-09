9c82628a9eb7e356bfc75fcb7b892fe8b59c093d
Roberto Reale <roberto@reale.me>
2019-03-09 03:25:42 +0100

    Edit program message

--- web/config.go
+++ web/config.go
@@ -14,6 +14,6 @@ var Conf *Config
 func InitialiseConfig() {
 	Conf = &Config{}
 	flag.IntVar(&Conf.Port, "p", 8080, "Port number to serve")
-	flag.StringVar(&Conf.APIKey, "apiKey", "", "The API Key for Alpha Vantage")
+	flag.StringVar(&Conf.APIKey, "apiKey", "", "The API Key for iPA")
 	flag.Parse()
 }
