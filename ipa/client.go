2b71f9ec7a191e7171fde7bf99bef30afced3587
Roberto Reale <roberto@reale.me>
2019-03-09 03:18:23 +0100

    Edit endpoint

--- ipa/client.go
+++ ipa/client.go
@@ -20,8 +20,8 @@ type Client struct {
 
 func NewClient(apiKey string) *Client {
 	c := &Client{}
-	c.BaseURL = "https://www.alphavantage.co"
-	c.Endpoint = "/query"
+	c.BaseURL = "https://www.indicepa.gov.it"
+	c.Endpoint = "/public-ws/WS01_SFE_CF.php"
 	c.APIKey = apiKey
 	return c
 }
